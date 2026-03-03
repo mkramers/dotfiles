5d8ebccf3082e073b88cd3ccd5822f07c8c9008f
mkramers <matt@altislabs.com>
2026-03-02 23:08:48 -0500

    feat: add LazyVim config with ayu-dark theme
    chore: add neovim to aqua packages

---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,12 @@
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "ayu" } },
+  checker = { enabled = true },
+})
